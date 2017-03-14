68cddefb1530892b612999dd887a510a958e007a
James Salamonsen <jamessalamonsen@gmail.com>
2017-03-14 13:20:36 -0700

    I added the rest of my functions that are working into my app.js and I added my app.js script to my sales.html.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+'use strict';
 //I wanted to use randomCustPerHour instead of my function.
 //var randomCustPerHour = Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
 //var avgCookHour =
+var body = document.getElementByTagName('body')[0];
+var total = 0;
 var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '8pm', 'total'];
 
 var pike = {
@@ -8,69 +11,158 @@ var pike = {
   maxCust: 65,
   avgCookCust: 6.3,
   hourlySales: [],
-  random: function() {
+  name: '1st and Pike.',
+  randomCust: function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
   },
   cookiesHourly: function() {
-    var randomCookies = Math.round(this.avgCookCust * this.random());
-    for(var i = 0; i <=14; i++) {
-      this.hourlySales.push(Math.round(randomCookies);
-    };
+    var cookiesPerHour = Math.floor(this.avgCookCust * this.random());
+    for(var i = 0; i < storeHours.length - 1; i++) {
+      this.hourlySales.push(Math.floor(cookiesPerHour);
+      total += cookiesPerHour;
+    }
+    this.hourlySales.push(total);
+  },
+  createListItems: function(){
+    this.cookiesHourly();
+    var newHeading = document.createElement('h2');
+    newHeading.innertext = this.name;
+    body.appendChild(newHeading);
+    var firstUl = document.getElement('ul');
+    for (var i = 0; i < storeHours.length -1; i++) {
+      var newEl = document.getElement('li');
+      newEl.innertext = storeHours[i] + ': ' + this.hourlySales[i] + ' cookies';
+      firstUl.appendChild(newEl);
+    }
+    body.appendChild(firstUl);
   }
-}
+};
+
+pike.createListItems();
 var seaTac = {
   minCust: 3,
   maxCust: 24,
   avgCookCust: 1.2,
-  random: function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust)
+  hourlySales: [],
+  randomCust: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
   },
   cookiesHourly: function() {
-    var randomCookies = Math.round(this.avgCookCust * this.random);
-    for(var i = 0; i <=14; i++) {
-      return randomCookies;
-    };
+    var cookiesPerHour = Math.floor(this.avgCookCust * this.random());
+    for(var i = 0; i < storeHours.length - 1; i++) {
+      this.hourlySales.push(Math.floor(cookiesPerHour);
+      total += cookiesPerHour;
+    }
+    this.hourlySales.push(total);
+  },
+  createListItems: function(){
+    this.cookiesHourly();
+    var newHeading = document.createElement('h2');
+    newHeading.innertext = this.name;
+    body.appendChild(newHeading);
+    var firstUl = document.getElement('ul');
+    for (var i = 0; i < storeHours.length -1; i++) {
+      var newEl = document.getElement('li');
+      newEl.innertext = storeHours[i] + ': ' + this.hourlySales[i] + ' cookies';
+      firstUl.appendChild(newEl);
+    }
+    body.appendChild(firstUl);
   }
 }
+
 var seaCenter = {
   minCust: 11,
   maxCust: 38,
   avgCookCust: 3.7,
-  random: function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust)
+  hourlySales: [],
+  randomCust: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
   },
   cookiesHourly: function() {
-    var randomCookies = Math.round(this.avgCookCust * this.random);
-    for(var i = 0; i <=14; i++) {
-      return randomCookies;
-    };
+    var cookiesPerHour = Math.floor(this.avgCookCust * this.random());
+    for(var i = 0; i < storeHours.length - 1; i++) {
+      this.hourlySales.push(Math.floor(cookiesPerHour);
+      total += cookiesPerHour;
+    }
+    this.hourlySales.push(total);
+  },
+  createListItems: function(){
+    this.cookiesHourly();
+    var newHeading = document.createElement('h2');
+    newHeading.innertext = this.name;
+    body.appendChild(newHeading);
+    var firstUl = document.getElement('ul');
+    for (var i = 0; i < storeHours.length -1; i++) {
+      var newEl = document.getElement('li');
+      newEl.innertext = storeHours[i] + ': ' + this.hourlySales[i] + ' cookies';
+      firstUl.appendChild(newEl);
+    }
+    body.appendChild(firstUl);
   }
+  seaCenter.createListItems();
 }
+
 var capHill = {
   minCust: 20,
   maxCust: 38,
   avgCookCust: 4.6,
-  random: function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust)
+  total: 0,
+  hourlySales: [],
+  randomCust: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
   },
   cookiesHourly: function() {
-    var randomCookies = Math.round(this.avgCookCust * this.random);
-    for(var i = 0; i <=14; i++) {
-      return randomCookies;
-    };
+    var cookiesPerHour = Math.floor(this.avgCookCust * this.random());
+    for(var i = 0; i < storeHours.length - 1; i++) {
+      this.hourlySales.push(Math.floor(cookiesPerHour);
+      total += cookiesPerHour;
+    }
+    this.hourlySales.push(total);
+  },
+  createListItems: function(){
+    this.cookiesHourly();
+    var newHeading = document.createElement('h2');
+    newHeading.innertext = this.name;
+    body.appendChild(newHeading);
+    var firstUl = document.getElement('ul');
+    for (var i = 0; i < storeHours.length -1; i++) {
+      var newEl = document.getElement('li');
+      newEl.innertext = storeHours[i] + ': ' + this.hourlySales[i] + ' cookies';
+      firstUl.appendChild(newEl);
+    }
+    body.appendChild(firstUl);
   }
+  capHill.createListItems();
 }
+
 var alki = {
   minCust: 2,
   maxCust: 16,
   avgCookCust: 4.6,
-  random: function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust)
+  hourlySales: [],
+  randomCust: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
   },
   cookiesHourly: function() {
-    var randomCookies = Math.round(this.avgCookCust * this.random);
-    for(var i = 0; i <=14; i++) {
-      return randomCookies;
-    };
+    var cookiesPerHour = Math.floor(this.avgCookCust * this.random());
+    for(var i = 0; i < storeHours.length - 1; i++) {
+      this.hourlySales.push(Math.floor(cookiesPerHour);
+      total += cookiesPerHour;
+    }
+    this.hourlySales.push(total);
+  },
+  createListItems: function(){
+    this.cookiesHourly();
+    var newHeading = document.createElement('h2');
+    newHeading.innertext = this.name;
+    body.appendChild(newHeading);
+    var firstUl = document.getElement('ul');
+    for (var i = 0; i < storeHours.length -1; i++) {
+      var newEl = document.getElement('li');
+      newEl.innertext = storeHours[i] + ': ' + this.hourlySales[i] + ' cookies';
+      firstUl.appendChild(newEl);
+    }
+    body.appendChild(firstUl);
   }
+  alki.createListItems();
 }
