b8a4f6f5b097a3e8e8af082ace92ce88e42b5499
James Salamonsen <jamessalamonsen@gmail.com>
2017-03-14 09:04:50 -0700

    This is my app.js with added push and store hours thanks to David Lim.

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 //I wanted to use randomCustPerHour instead of my function.
 //var randomCustPerHour = Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
 //var avgCookHour =
+var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '8pm', 'total'];
 
 var pike = {
   minCust: 23,
   maxCust: 65,
   avgCookCust: 6.3,
+  hourlySales: [],
   random: function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
   },
   cookiesHourly: function() {
-    var randomCookies = Math.round(this.avgCookCust * this.random);
+    var randomCookies = Math.round(this.avgCookCust * this.random());
     for(var i = 0; i <=14; i++) {
-      return randomCookies;
+      this.hourlySales.push(Math.round(randomCookies);
     };
   }
 }
